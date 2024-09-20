ff73029340ebc4d97ea2592fc1a8e8f7be639f2e
Otbaek <burhonovotabek5@gmil.com>
2024-09-20 12:03:13 +0500

    add changes to users

--- datas/data.js
+++ datas/data.js
@@ -1,8 +1,7 @@
-// datas/data.js
 const users = [
-    { uname: "testuser1", psw: "password123" },
-    { uname: "testuser2", psw: "password456" },
-    // boshqa foydalanuvchilar
+    { uname: "user1", psw: "password123" },
+    { uname: "user2", psw: "password456" },
+    { uname: "user3", psw: "password789" },
 ];
 
 module.exports = users;
